b696da609525991c5d20c2830d5e782d8da378a8
Nardiena Althafia Pratama <nardienapratama@yahoo.com>
2020-07-26 00:31:14 +1000

    set up prod environment using nginx and gunicorn

--- backend/main/app.py
+++ backend/main/app.py
@@ -6,9 +6,9 @@ from flask_mongoengine import MongoEngine
 
 # a simple Flask app using flask-mongoengine to connect to MongoDB
 
-appVar = Flask(__name__)
+app = Flask(__name__)
 
-appVar.config['MONGODB_SETTINGS'] = {
+app.config['MONGODB_SETTINGS'] = {
     'host' : os.environ['MONGODB_HOST'],
     'username': os.environ['MONGODB_USERNAME'],
     'password': os.environ['MONGODB_PASSWORD'],
@@ -16,7 +16,7 @@ appVar.config['MONGODB_SETTINGS'] = {
 }
 
 db = MongoEngine()
-db.init_app(appVar)
+db.init_app(app)
 
 class Todo(db.Document):
     title = db.StringField(max_length=60)
@@ -24,14 +24,14 @@ class Todo(db.Document):
     done = db.BooleanField(default=False)
     pub_date = db.DateTimeField(default=datetime.datetime.now)
 
-@appVar.route("/api")
+@app.route("/api")
 def index():
     Todo.objects().delete()
     Todo(title="Simple todo A", text="12345678910").save()
     Todo(title="Simple todo B", text="12345678910").save()
     Todo.objects(title__contains="B").update(set__text="Hello world")
     todos = Todo.objects().to__json()
-    return Response(todos, mimetype="application/json"), status=200)
+    return Response(todos, mimetype="application/json", status=200)
 
 if __name__ == "__main__":
     appVar.run(debug=True, port=5000)
