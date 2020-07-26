85527e0c16d8adafbcf1ec7f761e89103c6e24f2
Nardiena Althafia Pratama <nardienapratama@yahoo.com>
2020-07-27 03:31:54 +1000

    added buttons for each page, and components for each page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import Home from './components/Home';
+import Logs from './components/Logs';
+import Calendar from './components/Calendar';
+import Profile from './components/Profile';
 
-function App() {
- 
-  return (
-    <div className="container">
-      <div className="header-navbar">
-        <nav class="navbar navbar-dark bg-dark">
-          <span class="navbar-brand mb-0 h1">Navbar</span>
-        </nav>
-          
-      </div>
+export default class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 'home',
+    }
+  }
 
-      <div className="footer-navbar">
-        <nav class="navbar navbar-dark bg-dark">
-          <a class="navbar-brand" href="#">Fixed bottom</a>
-        </nav>
-      </div>
+  setToPage(page) {
+    this.setState({
+      page: page,
+    })
+  }
 
-    </div>
+  
+  render() {
+    const pageState = this.state.page;
+    let pageContent;
+    if (pageState == 'home') {
+      pageContent = <Home />
+    } else if (pageState == 'logs') {
+      pageContent = <Logs />
+    } else if (pageState == 'calendar') {
+      pageContent = <Calendar />
+    } else if (pageState == 'profile') {
+      pageContent = <Profile />
+    } 
     
-  );
+
+    return (
+      <div>
+        <div className="container">
+          <div className="header-navbar">
+            <nav class="navbar navbar-dark bg-dark">
+              <span class="navbar-brand mb-0 h1">Navbar</span>
+              <button class="btn" type="button">Search button</button>
+
+            </nav>
+              
+          </div>
+
+          <div>{pageContent}</div>
+
+          <div className="footer-navbar">
+            <nav class="navbar navbar-dark bg-dark">
+              <button class="btn" type="button" onClick={() => this.setToPage('home')}>Home</button>
+              <button class="btn" type="button" onClick={() => this.setToPage('logs')}>Practice Logs</button>
+              <button class="btn" type="button" onClick={() => this.setToPage('calendar')}>Calendar</button>
+              <button class="btn" type="button" onClick={() => this.setToPage('profile')}>Profile</button>
+            </nav>
+          </div>
+
+        </div>  
+      </div>
+      
+    );
+  }
+  
 }
 
-export default App;
