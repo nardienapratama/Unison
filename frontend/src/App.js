902a2231ff1e9820b50faf51d3101f50555b996f
Nardiena Althafia Pratama <nardienapratama@yahoo.com>
2020-07-27 00:14:08 +1000

    Deleted example files and deployed to AWS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,11 @@ import './App.css';
 import { PracticeRecords } from './components/PracticeRecords';
 
 function App() {
-
-  const [practiceRecords, setPracticeRecords] = useState([]);
-
-  useEffect(() => {
-    fetch('/practicerecords').then(response => response.json()).then(data => {
-      console.log(data);
-      setPracticeRecords(data.practiceRecords);
-    });
-  }, []);
-
-
-  console.log(practiceRecords)
-  
+ 
   return (
     <div className="App">
       
-    <PracticeRecords PracticeRecordsArg={practiceRecords}/>
+    
     </div>
   );
 }
