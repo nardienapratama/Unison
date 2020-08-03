787500c22d3d649a98a0eb5fa8315ba0dc5aed40
Nardiena Pratama <nardienapratama@yahoo.com>
2020-08-04 01:59:17 +1000

    added react router links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import Home from './components/Home';
 import Logs from './components/Logs';
 import Calendar from './components/Calendar';
 import Profile from './components/Profile';
+import { Route, Link } from "react-router-dom";
 
 export default class App extends React.Component{
   constructor(props) {
@@ -20,40 +21,32 @@ export default class App extends React.Component{
   }
 
   
-  render() {
-    const pageState = this.state.page;
-    let pageContent;
-    if (pageState == 'home') {
-      pageContent = <Home />
-    } else if (pageState == 'logs') {
-      pageContent = <Logs />
-    } else if (pageState == 'calendar') {
-      pageContent = <Calendar />
-    } else if (pageState == 'profile') {
-      pageContent = <Profile />
-    } 
-    
-
+  render() { 
     return (
       <div>
         <div className="container">
           <div className="header-navbar">
-            <nav class="navbar navbar-dark bg-dark">
-              <span class="navbar-brand mb-0 h1">Navbar</span>
-              <button class="btn" type="button">Search button</button>
+            <nav className="navbar navbar-dark bg-dark">
+              <span className="navbar-brand mb-0 h1">Navbar</span>
+              <Link to="/search"><button className="btn" type="button">Search button</button></Link>
 
             </nav>
               
           </div>
 
-          <div>{pageContent}</div>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/logs" component={Logs}/>
+          <Route exact path="/calendar" component={Calendar}/>
+          <Route exact path="/profile" component={Profile}/>
+
+
 
           <div className="footer-navbar">
-            <nav class="navbar navbar-dark bg-dark">
-              <button class="btn" type="button" onClick={() => this.setToPage('home')}>Home</button>
-              <button class="btn" type="button" onClick={() => this.setToPage('logs')}>Practice Logs</button>
-              <button class="btn" type="button" onClick={() => this.setToPage('calendar')}>Calendar</button>
-              <button class="btn" type="button" onClick={() => this.setToPage('profile')}>Profile</button>
+            <nav className="navbar navbar-dark bg-dark">
+              <Link to="/"><button className="btn" type="button" onClick={() => this.setToPage('home')}>Home</button></Link>
+              <Link to="/logs"><button className="btn" type="button" onClick={() => this.setToPage('logs')}>Practice Logs</button></Link>
+              <Link to="/calendar"><button className="btn" type="button" onClick={() => this.setToPage('calendar')}>Calendar</button></Link>
+              <Link to="/profile"><button className="btn" type="button" onClick={() => this.setToPage('profile')}>Profile</button></Link>
             </nav>
           </div>
 
